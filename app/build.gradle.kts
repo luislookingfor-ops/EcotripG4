29663b73aa74a95b007d8b1a6589fe5b345e8c6d
JORGE LUIS <jlcorrales@puce.edu.ec>
2026-04-15 13:41:54 -0500

    feat: data model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,4 +30,18 @@ android {
     buildFeatures {
         compose = true
     }
+
+}
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.activity.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3) //
 }
